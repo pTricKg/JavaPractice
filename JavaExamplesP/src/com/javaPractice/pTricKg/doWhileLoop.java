7a67e0b14ec67d1c59d529607ab4fb840edaef33
pTricKg <ptrickg1976@gmail.com>
2012-08-16 12:06:31 -0600

    doWhile

---
+++ JavaExamplesP/src/com/javaPractice/pTricKg/doWhileLoop.java
@@ -0,0 +1,28 @@
+package com.javaPractice.pTricKg;
+import java.io.File;
+import java.util.Scanner;
+import static java.lang.System.out;
+
+
+public class doWhileLoop {
+	public static void main(String args[]) {
+		File books = new File("badBooks.txt");
+		Scanner keyboard = new Scanner(System.in);
+		char reply;
+		
+		do {
+			out.print("Delete books? (y/n) ");
+			reply = keyboard.findWithinHorizon(".", 0).charAt(0);
+		}while (reply != 'y' && reply != 'Y' &&
+				reply != 'n' && reply != 'N');
+		
+		if (reply == 'y' || reply == 'Y') {
+			out.println("Deleting per your request...");
+			books.delete();
+			out.println("The books have been deleted.");
+			
+		}else {
+			out.println("Need to confirm!");
+		}
+	}
+}
--- JavaExamplesP/src/com/javaPractice/pTricKg/guessWindow.java
+++ JavaExamplesP/src/com/javaPractice/pTricKg/guessWindow.java
@@ -1,13 +1,13 @@
 package com.javaPractice.pTricKg;
 import java.util.Random;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 
 public class guessWindow {
 	public static void main(String args[]) {
-		Scanner pickNumber = new Scanner (System.in);
+		//Scanner pickNumber = new Scanner (System.in);
 		
 		int pickAnumber = Integer.parseInt(JOptionPane.showInputDialog("Pick an integer from 1 to 10"));
 		//int inputNumber = pickNumber.nextInt();
@@ -17,7 +17,7 @@ public class guessWindow {
 		if (pickAnumber == randomNumber) {	
 			JOptionPane.showMessageDialog(null, "Hooray! You got it right!!");
 		}else {
-			JOptionPane.showMessageDialog(null, "I was thinking of " + randomNumber);
+			//JOptionPane.showMessageDialog(null, "I was thinking of " + randomNumber);
 			JOptionPane.showInputDialog("Try again!");
 		}
 		JOptionPane.showMessageDialog(null, "Thanks for playing!");
