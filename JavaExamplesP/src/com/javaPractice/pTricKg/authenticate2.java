0e6cc5b2d7916387a81b8a57a716d74cf3553737
pTricKg <ptrickg1976@gmail.com>
2015-05-20 23:06:01 -0600

    add comment

--- JavaExamplesP/src/com/javaPractice/pTricKg/authenticate2.java
+++ JavaExamplesP/src/com/javaPractice/pTricKg/authenticate2.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 class authenticate2 {
 	
 	public static void main(String args[]) {
+		// ask for credentials
 		Scanner keyboard = new Scanner(System.in);
 		
 		out.print("Username: ");
