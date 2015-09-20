1ba5d64f74ec00098c3e75fe9d9921a4ea50e4ca
ptrickg <ptrickg1976@gmail.com>
2015-09-20 01:25:07 -0600

    check letter for vowels

---
+++ WordCheckLetter/Word.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+/**
+ * Simply checking whether letter in given word
+ * is a vowel or consonant
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Word
+{
+    private String letters;
+
+    public static void main(String[] args)
+    {   
+        String v = " is vowel";
+
+        Scanner word = new Scanner(System.in);
+        System.out.print("Give me a word and i'll tell you if its a vowel: ");
+        String user = word.nextLine();
+
+        System.out.println("You input: " + user);
+        user = user.toLowerCase();//.replaceAll("\\s","");
+        System.out.println(user);
+
+        int l;
+        for (l = 0; l < user.length(); l++)
+        {
+            System.out.println("Letter of your word at index " + l);
+            if (user.substring(l, l + 1).equals("a"))
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(v);
+            }
+            else if (user.substring(l, l + 1).equals("e"))
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(v);
+            }
+            else if (user.substring(l, l + 1).equals("i"))
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(v);
+            }
+            else if (user.substring(l, l + 1).equals("o"))
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(v);
+            }
+            else if (user.substring(l, l + 1).equals("u"))
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(v);
+            }
+            else 
+            {
+                System.out.print(user.substring(l, l + 1));
+                System.out.println(" is consonant");
+            }
+        }
+
+        //         System.out.println(user.isVowel(0));
+        //         System.out.println(user.isVowel(1));
+        //         System.out.println(user.isVowel(2));
+        //         System.out.println(user.isVowel(3));
+        //         System.out.println(user.isVowel(4));
+    }
+
+    
+}
