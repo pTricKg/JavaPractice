ded74eea5911af9e5978fd820a18072926d16435
ptrickg <ptrickg1976@gmail.com>
2015-09-21 00:06:16 -0600

    add new folder

---
+++ Work15/Pluralize/PluralizeWord.java
@@ -0,0 +1,87 @@
+
+public class PluralizeWord
+{
+    private String letters;
+
+    public PluralizeWord(String letters)
+    {
+        this.letters = letters.toLowerCase();
+    }
+
+    /**
+    Forms the plural of this word.
+    @return the plural, using the rules for regular nouns
+     */
+    public String getPluralForm()
+    {
+        // y preceded by a consonant, add ies.
+        // y preceded by a vowel, add an s.
+        // o, s, sh, or ch, add es
+        // or add an s.
+        int last = letters.length() - 1; //get last letter
+
+        if (is(last, "y")) //plural y 
+        {
+            if (isConsonant(letters.length() - 2)) // with consonant
+            {
+                return letters.substring(0, last) + "ies";
+            }
+            else // with vowel
+            {
+                return letters + "s";
+            }
+        }
+        else if (is(last, "o") || is(last, "s")) // plural o.s,sh,ch
+        {
+            return letters + "es";
+        }
+        else if (is(last, "h"))
+        {
+            if (is(letters.length() - 2, "s") || is(letters.length() - 2, "c"))
+            {
+                return letters + "es";
+            }
+            else
+            {
+                return letters + "s";
+            }
+        }
+        return letters + "s";
+    }
+
+    /**
+    Tests whether the ith letter is a vowel.
+    @param i the index of the letter to test
+    @return true if the ith letter is a vowel
+     */
+    public boolean isVowel(int i)
+    {
+        return is(i, "a")
+        || is(i, "e")
+        || is(i, "i")
+        || is(i, "o")
+        || is(i, "u");
+    }
+
+    /**
+    Tests whether the ith letter is a consonant.
+    @param i the index of the letter to test
+    @return true if the ith letter is a consonant
+     */
+    public boolean isConsonant(int i)
+    {
+        return !isVowel(i);
+    }
+
+    /**
+     * Checks what letter is in position i
+     * @return true when the the letter of letters is the given letter.
+     *         false otherwise.
+     * @param i index in letters
+     * @param letter the letter to match with. must be one character long.
+     */
+    public boolean is(int i, String letter)
+    {
+        return letters.substring(i, i + 1).equals(letter);
+    }
+}
---
+++ Work15/Pluralize/PluralizeWordTest.java
@@ -0,0 +1,43 @@
+
+public class PluralizeWordTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new PluralizeWord("kiss").getPluralForm());
+        System.out.println("Expected: kisses");
+        System.out.println(new PluralizeWord("phase").getPluralForm());
+        System.out.println("Expected: phases");
+        System.out.println(new PluralizeWord("dish").getPluralForm());
+        System.out.println("Expected: dishes");
+        System.out.println(new PluralizeWord("massage").getPluralForm());
+        System.out.println("Expected: massages");
+        System.out.println(new PluralizeWord("witch").getPluralForm());
+        System.out.println("Expected: witches");
+        System.out.println(new PluralizeWord("judge").getPluralForm());
+        System.out.println("Expected: judges");
+        System.out.println(new PluralizeWord("lap").getPluralForm());
+        System.out.println("Expected: laps");
+        System.out.println(new PluralizeWord("cat").getPluralForm());
+        System.out.println("Expected: cats");
+        System.out.println(new PluralizeWord("clock").getPluralForm());
+        System.out.println("Expected: clocks");
+        System.out.println(new PluralizeWord("cuff").getPluralForm());
+        System.out.println("Expected: cuffs");
+        System.out.println(new PluralizeWord("death").getPluralForm());
+        System.out.println("Expected: deaths");
+        System.out.println(new PluralizeWord("boy").getPluralForm());
+        System.out.println("Expected: boys");
+        System.out.println(new PluralizeWord("girl").getPluralForm());
+        System.out.println("Expected: girls");
+        System.out.println(new PluralizeWord("chair").getPluralForm());
+        System.out.println("Expected: chairs");
+        System.out.println(new PluralizeWord("day").getPluralForm());
+        System.out.println("Expected: days");
+        System.out.println(new PluralizeWord("monkey").getPluralForm());
+        System.out.println("Expected: monkeys");
+        System.out.println(new PluralizeWord("bath").getPluralForm());
+        System.out.println("Expected: baths");
+        System.out.println(new PluralizeWord("cherry").getPluralForm());
+        System.out.println("Expected: cherries");
+    }
+}
