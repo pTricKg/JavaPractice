dd9df8b12b95c70d2191fde90ed740c2fb7c9030
ptrickg <ptrickg1976@gmail.com>
2016-01-15 23:35:16 -0700

    matching numbers in long

---
+++ Work16/UdacityWork/Number.java
@@ -0,0 +1,45 @@
+// Bluej project: lesson6/counting_matches_digits
+/**
+ * A class that represents a number and can report
+ * properties of the number.
+ */
+public class Number
+{
+    private long number;
+
+    public Number(long number)
+    {
+        this.number = number;
+    }
+
+    /**
+     * Returns the number of times digitToMatch appears
+     * in this number.
+     * @param digitToMatch the digit to search for.
+     *   must be at least 0 and at most 9.
+     * @return number of occurrences of digitToMatch
+     */
+    public int countMatchingDigits(int digitToMatch)
+    {
+        // TODO: count and return the number of times the given digit appears in the number
+
+        // Hint: The last digit is (int)(numbers % 10).
+        int count = 0;
+        long temp = number;
+        while(temp > 0)
+        {
+            int digit = (int)(temp % 10); // cast int on long
+            System.out.println("Pop this " + digit);
+            if (digit == digitToMatch)
+            {
+                count++;
+                System.out.println("And we have a winner: " + digitToMatch);
+            }
+            temp = temp / 10; // pop off last digit of long for comparison
+            System.out.println("After pop " + temp);
+                   
+        }
+        
+        return count;
+    }
+}
